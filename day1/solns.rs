50f95aecebdd15cc774bd6a7e0a1e4e1bb4eb1fd
Ray Zhang <>
2022-12-03 00:07:19 -0800

    Move day1 to use utils/fs.rs

--- day1/solns.rs
+++ day1/solns.rs
@@ -1,19 +1,4 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::Result;
-use std::io::{Error, ErrorKind};
-
-fn readfile(fp: &str) -> Result<String> {
-    let mut f = File::open(fp)?;
-    let mut buf = String::new();
-    match f.read_to_string(&mut buf) {
-        Err(_) => Err(Error::new(
-            ErrorKind::NotFound,
-            format!("file {} not found", fp),
-        )),
-        Ok(_) => Ok(buf),
-    }
-}
+use crate::utils::fs;
 
 fn parse_input(buf: String) -> Vec<Vec<u32>> {
     let parts = buf.split("\n");
@@ -34,14 +19,13 @@ fn parse_input(buf: String) -> Vec<Vec<u32>> {
 }
 
 pub fn solution(fp: &str) {
-    let input = match readfile(fp) {
+    let parsed = match fs::parse_file(fp, parse_input) {
         Err(_) => {
             println!("file not found");
             return;
         }
-        Ok(buf) => buf,
+        Ok(parsed) => parsed,
     };
-    let parsed = parse_input(input);
     let mut sums: Vec<u32> = parsed
         .iter()
         .map(|vl| -> u32 { vl.iter().fold(0, |acc, v| -> u32 { acc + v }) })
