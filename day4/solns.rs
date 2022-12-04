f55240e9f1a91b1c7f41e580519dde5b0701486f
Ray Zhang <>
2022-12-04 00:31:05 -0800

    Initial commit for day 4

---
+++ day4/solns.rs
@@ -0,0 +1,50 @@
+use regex::Regex;
+use crate::utils::fs;
+
+fn parse_input(buf: String) -> Vec<(u32, u32, u32, u32)> {
+    let pattern = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
+    let mut res = Vec::new();
+    for grp in pattern.captures_iter(&buf) {
+        res.push(
+            (grp[1].parse::<u32>().unwrap(),
+            grp[2].parse::<u32>().unwrap(),
+            grp[3].parse::<u32>().unwrap(),
+            grp[4].parse::<u32>().unwrap()));
+    }
+    res
+}
+
+pub fn solution1(fp: &str) -> u32 {
+    let parsed = match fs::parse_file(fp, parse_input) {
+        Err(e) => {
+            println!("{:?}", e);
+            return 0;
+        },
+        Ok(parsed) => parsed,
+    };
+
+    parsed.iter().fold(0, |acc, (lp1h, lp1t, lp2h, lp2t)|
+        if (lp1h <= lp2h && lp1t >= lp2t) || (lp1h >= lp2h && lp1t <= lp2t) {
+            acc + 1
+        } else {
+            acc
+        }
+    )
+}
+
+pub fn solution2(fp: &str) -> u32 {
+    let parsed = match fs::parse_file(fp, parse_input) {
+        Err(e) => {
+            println!("{:?}", e);
+            return 0;
+        },
+        Ok(parsed) => parsed,
+    };
+    parsed.iter().fold(0, |acc, (lp1h, lp1t, lp2h, lp2t)|
+        if lp1t >= lp2h && lp1h <= lp2t {
+            acc + 1
+        } else {
+            acc
+        }
+    )
+}
--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
-mod day3;
+mod day4;
 mod utils;
 
 fn main() {
-    let fp = "day3/input.txt";
-    println!("soln 1: {}", day3::solns::solution1(fp));
-    println!("soln 2: {}", day3::solns::solution2(fp));
+    let fp = "day4/input.txt";
+    println!("soln 1: {}", day4::solns::solution1(fp));
+    println!("soln 2: {}", day4::solns::solution2(fp));
 }
